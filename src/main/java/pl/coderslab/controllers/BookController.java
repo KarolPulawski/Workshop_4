6c0c8e2f33545ee454d3d08a3bcfb9ffe854c1fa
KarolPulawski <karol.j.pulawski@gmail.com>
2018-08-10 15:35:00 +0200

    Workshop 4 / adress: /books - return all books.

--- src/main/java/pl/coderslab/controllers/BookController.java
+++ src/main/java/pl/coderslab/controllers/BookController.java
@@ -1,12 +1,20 @@
 package pl.coderslab.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.model.Book;
+import pl.coderslab.service.MemoryBookService;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
+
+    @Autowired
+    private MemoryBookService memoryBookService;
 
     @RequestMapping("/hello")
     public String hello(){
@@ -18,6 +26,13 @@ public class BookController {
         return new Book(1L,"9788324631766","Thinking in Java",
                 "Bruce Eckel","Helion","programming");
     }
+
+    @RequestMapping("/books")
+    @ResponseBody
+    public List<Book> displayAllBooks() {
+        return memoryBookService.getList();
+    }
+
 
 
 
--- src/main/java/pl/coderslab/service/MemoryBookService.java
+++ src/main/java/pl/coderslab/service/MemoryBookService.java
@@ -1,10 +1,13 @@
 package pl.coderslab.service;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import pl.coderslab.model.Book;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 @Service
 public class MemoryBookService {
